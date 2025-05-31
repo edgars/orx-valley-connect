496e5f5669cef5ca03b0549e3827a56ddb408993
wagner <wagner@qriarlabs.com>
2025-05-31 18:49:27 -0300

    Changing credentials

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,13 +2,13 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://dwbvvnvdcomlgvxuokxd.supabase.co";
+/* const SUPABASE_URL = "https://dwbvvnvdcomlgvxuokxd.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3YnZ2bnZkY29tbGd2eHVva3hkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3ODI5NTEsImV4cCI6MjA1ODM1ODk1MX0.J91ujeywJD6UdBx-LRaWEPE6QY20fBSPLb984y9OT1s";
-
-
-/* const SUPABASE_URL = "https://xeqfordyobjphmvswqbb.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlcWZvcmR5b2JqcGhtdnN3cWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NDk3ODMsImV4cCI6MjA2NDIyNTc4M30.sOAA521eD_6tJoSkBqxV7HrPxskvFMUZWhg1uxLcTd8";
  */
+
+const SUPABASE_URL = "https://xeqfordyobjphmvswqbb.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlcWZvcmR5b2JqcGhtdnN3cWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NDk3ODMsImV4cCI6MjA2NDIyNTc4M30.sOAA521eD_6tJoSkBqxV7HrPxskvFMUZWhg1uxLcTd8";
+
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
